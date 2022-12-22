75f67e30cd972491656e15c1e256597fcc6f975d
Alex Collins <alex_collins@intuit.com>
2022-12-22 06:31:23 +0000

    Brew formula update for joy version v0.0.6

--- Formula/joy.rb
+++ Formula/joy.rb
@@ -5,20 +5,20 @@
 class Joy < Formula
   desc "Joyful local dev."
   homepage "https://github.com/alexec/joy"
-  version "0.0.4"
+  version "0.0.6"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/alexec/joy/releases/download/v0.0.4/joy_0.0.4_Darwin_x86_64.tar.gz"
-      sha256 "3ae50c19538107ce6d22db000b04d4f73a5acb166dac8153ad6eba8e487b5a83"
+      url "https://github.com/alexec/joy/releases/download/v0.0.6/joy_0.0.6_Darwin_x86_64.tar.gz"
+      sha256 "5748d8f117d6ab04c178f39daad3901060079299f365935ff7686177220a410e"
 
       def install
         bin.install "joy"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/alexec/joy/releases/download/v0.0.4/joy_0.0.4_Darwin_arm64.tar.gz"
-      sha256 "564c1bc3e8371799620d600a69c02eff9ca8ee0398d32e25cf273695b354fb24"
+      url "https://github.com/alexec/joy/releases/download/v0.0.6/joy_0.0.6_Darwin_arm64.tar.gz"
+      sha256 "1846452f5c13ea452c31027d43caf71f2362e785be07f29eec6d9e3e1eedb99e"
 
       def install
         bin.install "joy"
@@ -28,16 +28,16 @@ class Joy < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/alexec/joy/releases/download/v0.0.4/joy_0.0.4_Linux_arm64.tar.gz"
-      sha256 "455a85e586e7ca82a2c57d00bcccae2dd96769941cfc95f13462f7d49265d9de"
+      url "https://github.com/alexec/joy/releases/download/v0.0.6/joy_0.0.6_Linux_arm64.tar.gz"
+      sha256 "ecbee754f949bd41381cbaed917c9cd54052fb2e895765ffaedcbfeacb06a4f3"
 
       def install
         bin.install "joy"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/alexec/joy/releases/download/v0.0.4/joy_0.0.4_Linux_x86_64.tar.gz"
-      sha256 "69ee048115fd624c662f51c3a91a7ca0c263f8c0a6c1261ffa3190e27044332a"
+      url "https://github.com/alexec/joy/releases/download/v0.0.6/joy_0.0.6_Linux_x86_64.tar.gz"
+      sha256 "a633655c4036f85900c967f78d4e87b3ce711e7e35ce92450313b4e1caaa65fb"
 
       def install
         bin.install "joy"
