d63b35b174e0953c5207beba9c6404f5edd9e2aa
Alex Collins <alex_collins@intuit.com>
2023-02-04 22:33:51 +0000

    Brew formula update for kit version v0.0.30

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -5,20 +5,20 @@
 class Kit < Formula
   desc "Kitful local dev."
   homepage "https://github.com/alexec/kit"
-  version "0.0.29"
+  version "0.0.30"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/alexec/kit/releases/download/v0.0.29/kit_0.0.29_Darwin_x86_64.tar.gz"
-      sha256 "4d26a7e175f6c542a9bc2600270268439e6e01fd7027acdc2df8e1c900863583"
+      url "https://github.com/alexec/kit/releases/download/v0.0.30/kit_0.0.30_Darwin_x86_64.tar.gz"
+      sha256 "b207906808c4d30aa9162f8cb925335a1a6d02e89e0578cbbf51775e8a581713"
 
       def install
         bin.install "kit"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/alexec/kit/releases/download/v0.0.29/kit_0.0.29_Darwin_arm64.tar.gz"
-      sha256 "3f99e5773a03db46323b0e342eaca601a6b3197ed9415dfb9df6f832ddfa2922"
+      url "https://github.com/alexec/kit/releases/download/v0.0.30/kit_0.0.30_Darwin_arm64.tar.gz"
+      sha256 "f0809c32a6afacd18c8d01fbfec4617f828660a86ce9178399d185aed1fefe93"
 
       def install
         bin.install "kit"
@@ -28,16 +28,16 @@ class Kit < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/alexec/kit/releases/download/v0.0.29/kit_0.0.29_Linux_arm64.tar.gz"
-      sha256 "8f6fc61812263285874dc2b390507414c4fe0d6d66be628657b8b9716e549ff8"
+      url "https://github.com/alexec/kit/releases/download/v0.0.30/kit_0.0.30_Linux_arm64.tar.gz"
+      sha256 "3268fb0b2ee7ff43245b448bb495e82eb30cb88a30a4889983e0a5cdc78270e6"
 
       def install
         bin.install "kit"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/alexec/kit/releases/download/v0.0.29/kit_0.0.29_Linux_x86_64.tar.gz"
-      sha256 "e8b3d6377a180efc5a50ce856736ea633ece2e4bbf8cd21290dd4e20e1e21624"
+      url "https://github.com/alexec/kit/releases/download/v0.0.30/kit_0.0.30_Linux_x86_64.tar.gz"
+      sha256 "7a00f7732f4abea732a0e7f7c93b39be9a7b4dc09ea18a4ac75caa44812cfc52"
 
       def install
         bin.install "kit"
