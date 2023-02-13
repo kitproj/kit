1771783d95ae4e0cb2ae4f984b3b5836b2bdd421
Alex Collins <alex_collins@intuit.com>
2023-02-12 20:55:04 -0800

    fix: quoted

--- internal/types/strings_test.go
+++ internal/types/strings_test.go
@@ -6,9 +6,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestStrings_MarshalJSON(t *testing.T) {
-	x := Strings{"a", `"b"`}
-	data, err := x.MarshalJSON()
-	assert.NoError(t, err)
-	assert.Equal(t, `"a \"\"\"b\"\"\""`, string(data))
+func TestStrings(t *testing.T) {
+	t.Run("MarshalJSON", func(t *testing.T) {
+		x := Strings{"a", `"b"`}
+		data, err := x.MarshalJSON()
+		assert.NoError(t, err)
+		assert.Equal(t, `"a \"\"\"b\"\"\""`, string(data))
+	})
+	t.Run("UnmarshalJSON", func(t *testing.T) {
+		s := &Strings{}
+		err := s.UnmarshalJSON([]byte("a 'b'"))
+		assert.NoError(t, err)
+	})
 }
