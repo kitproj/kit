04cfc7f6e1cff3fc5b10d950b84d1c421cb4dfb7
Alex Collins <alex_collins@intuit.com>
2023-02-19 10:12:40 -0800

    feat: add install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -eux
 
 tag=$(curl -s "https://api.github.com/repos/alexec/kit/releases/latest" | jq -r '.tag_name')
-version=$(echo $tag | cut -c 1)
+version=$(echo $tag | cut -c 1-)
 url="https://github.com/alexec/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m).tar.gz"
 curl -Lo kit.tar.gz $url
 tar -xvf kit.tar.gz kit
