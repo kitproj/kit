9c2270119b9a482897e3567bc52eaa67126dbf57
Alex Collins <alex_collins@intuit.com>
2023-02-21 14:49:43 -0800

    build: do not download to local file
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ set -eux
 tag=$(curl -s "https://api.github.com/repos/alexec/kit/releases/latest" | jq -r '.tag_name')
 version=$(echo $tag | cut -c 2-)
 url="https://github.com/alexec/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m).tar.gz"
-curl -L $url | tar -xvf - kit
+curl -L $url | tar -zxvf - kit
 chmod +x kit
 sudo mv kit /usr/local/bin/kit
