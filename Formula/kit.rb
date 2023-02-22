982084ed63581567d457215a927186d7c8885362
Alex Collins <alex_collins@intuit.com>
2023-02-22 16:29:43 +0000

    Brew formula update for kit version v0.0.52

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -5,20 +5,20 @@
 class Kit < Formula
   desc "Kitful local dev."
   homepage "https://github.com/alexec/kit"
-  version "0.0.51"
+  version "0.0.52"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/alexec/kit/releases/download/v0.0.51/kit_0.0.51_Darwin_x86_64.tar.gz"
-      sha256 "47741295b7bd8847a36a2c6602a276c7d287b356ae2c252bc63e50a0ca4554e5"
+    if Hardware::CPU.arm?
+      url "https://github.com/alexec/kit/releases/download/v0.0.52/kit_0.0.52_Darwin_arm64.tar.gz"
+      sha256 "2e8849dc2612f8accccfdb798cd313a3a291e4fbd592061ba997bbeb5a9935df"
 
       def install
         bin.install "kit"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/alexec/kit/releases/download/v0.0.51/kit_0.0.51_Darwin_arm64.tar.gz"
-      sha256 "3b54af292330d33355d2d18ee624b872167fe2323b70e963c4c801abf9104849"
+    if Hardware::CPU.intel?
+      url "https://github.com/alexec/kit/releases/download/v0.0.52/kit_0.0.52_Darwin_x86_64.tar.gz"
+      sha256 "cafc316e6a6f0fc66ea93a5971552abc95791392e742ee2d8163388ad7c26a53"
 
       def install
         bin.install "kit"
@@ -28,16 +28,16 @@ class Kit < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/alexec/kit/releases/download/v0.0.51/kit_0.0.51_Linux_arm64.tar.gz"
-      sha256 "12b0ad9764347efeb352267170b2393278073503864cc95c2548e35778017117"
+      url "https://github.com/alexec/kit/releases/download/v0.0.52/kit_0.0.52_Linux_arm64.tar.gz"
+      sha256 "f04c2bd5bedac02420793df2abbe700cef0f5501b7efa8a85826f6ff394c9e52"
 
       def install
         bin.install "kit"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/alexec/kit/releases/download/v0.0.51/kit_0.0.51_Linux_x86_64.tar.gz"
-      sha256 "87f15843156992fe3991ea698c4bddbdad015077b1fbffe50b985107b2835630"
+      url "https://github.com/alexec/kit/releases/download/v0.0.52/kit_0.0.52_Linux_x86_64.tar.gz"
+      sha256 "56cf6267cf612c304d2c43b2b1a0949d7330d7472852bc1d4a38081951dc7f4d"
 
       def install
         bin.install "kit"
