1beea4d64fa17050b7cc84dbdb75a831d2f43aa7
Alex Collins <alex_collins@intuit.com>
2023-03-29 14:53:01 -0700

    fix: update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env sh
 set -eux
 
-tag=
-while [ "$tag" = "" ]; do
-  tag=$(curl -fsL "https://api.github.com/repos/kitproj/kit/releases/latest" | jq -r '.tag_name')
-done
+# retry for all errors up to 10m
+curl --retry 99 --retry-all-errors -fsL "https://api.github.com/repos/kitproj/kit/releases/latest" -o /tmp/latest
 
+tag=$(jq -r .tag_name /tmp/latest)
 version=$(echo $tag | cut -c 2-)
 url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
 
-while [ ! -e kit ]; do
-  curl -fsL $url | tar -zxvf - kit
-done
-
+# retry for all errors up to 10m
+curl --retry 99 --retry-all-errors -fsL $url -o /tmp/kit.tar.gz
+tar -zxvf /tmp/kit.tar.gz kit
 chmod +x kit
 sudo mv kit /usr/local/bin/kit
