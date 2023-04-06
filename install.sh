e4c04b340e86513f1d4170b0b576069ed2eaadba
Alex Collins <alex_collins@intuit.com>
2023-04-06 08:17:08 -0700

    build: allow you to specify version
    Signed-off-by: Alex Collins <alex_collins@intuit.com>

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 set -eux
 
-# retry for all errors up to 10m
-curl --retry 99 -vfsL "https://api.github.com/repos/kitproj/kit/releases/latest" -o /tmp/latest
+tag=${tag:-}
 
-tag=$(jq -r .tag_name /tmp/latest)
+if [ "$tag" = "" ]; then
+  # retry for all errors up to 10m
+  curl --retry 99 -vfsL "https://api.github.com/repos/kitproj/kit/releases/latest" -o /tmp/latest
+
+  tag=$(jq -r .tag_name /tmp/latest)
+fi
+
 version=$(echo $tag | cut -c 2-)
+
 url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
 
 # retry for all errors up to 10m
