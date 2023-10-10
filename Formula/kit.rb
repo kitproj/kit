e0d44251b9192c4624f7d046e9af88e51c54b84a
Alex Collins <alex_collins@intuit.com>
2023-10-10 03:28:38 +0000

    Brew formula update for kit version v0.1.14

--- Formula/kit.rb
+++ Formula/kit.rb
@@ -5,20 +5,20 @@
 class Kit < Formula
   desc "Crazy fast local dev loop."
   homepage "https://github.com/kitproj/kit"
-  version "0.1.13"
+  version "0.1.14"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kitproj/kit/releases/download/v0.1.13/kit_0.1.13_Darwin_x86_64.tar.gz"
-      sha256 "7f0e96c1bc15f61208ce771fe5f6b9704265bc1a7d608bb38b3be8efc1a31d3c"
+      url "https://github.com/kitproj/kit/releases/download/v0.1.14/kit_0.1.14_Darwin_x86_64.tar.gz"
+      sha256 "ac842cc16184adeac1929a8e67d44388c2e71c7854de5c0efa19ad3551b4b8d8"
 
       def install
         bin.install "kit"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/kitproj/kit/releases/download/v0.1.13/kit_0.1.13_Darwin_arm64.tar.gz"
-      sha256 "2f009dd6b2b0f839808ed2a9038c5b167088c8c955f0cf8bef2f0e8e4f121650"
+      url "https://github.com/kitproj/kit/releases/download/v0.1.14/kit_0.1.14_Darwin_arm64.tar.gz"
+      sha256 "166966bcb110722f716d95e9cfcd62f1733077016f6f879b8da72701f34089ae"
 
       def install
         bin.install "kit"
@@ -28,16 +28,16 @@ class Kit < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kitproj/kit/releases/download/v0.1.13/kit_0.1.13_Linux_arm64.tar.gz"
-      sha256 "23ea76c71991155bd549c5934518493bc7e6f7ea1c1d5b534489ff6fcd92d953"
+      url "https://github.com/kitproj/kit/releases/download/v0.1.14/kit_0.1.14_Linux_arm64.tar.gz"
+      sha256 "8a33ae1bce7fe22502f67fba0b257364b39f6600b3f5142dba416ed376085ca8"
 
       def install
         bin.install "kit"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/kitproj/kit/releases/download/v0.1.13/kit_0.1.13_Linux_x86_64.tar.gz"
-      sha256 "5642475b693c452c132b8bf64f9723dd391ec2e4981049884638fa231123bc24"
+      url "https://github.com/kitproj/kit/releases/download/v0.1.14/kit_0.1.14_Linux_x86_64.tar.gz"
+      sha256 "cac269900043bea2126fd1564d0b291c01ad9a1448e91a535a1ffc07e4574f77"
 
       def install
         bin.install "kit"
