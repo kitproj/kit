388cc434caab06f0bc7683fb4714121d26b2bdbe
Alex Collins <alex_collins@intuit.com>
2024-12-26 12:15:53 -0800

    feat: simplify install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
-set -eux
+set -eu
 
 tag=${tag:-}
 
 if [ "$tag" = "" ]; then
   # retry for all errors up to 10m
-  curl --retry 99 -vfsL "https://api.github.com/repos/kitproj/kit/releases/latest" -o /tmp/latest
+  curl --fail --retry 99 --silent --location https://api.github.com/repos/kitproj/kit/releases/latest --output /tmp/latest
 
   tag=$(jq -r .tag_name /tmp/latest)
 fi
 
 version=$(echo $tag | cut -c 2-)
 
+echo "Downloading Kit $version"
+
 url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
 
 # retry for all errors up to 10m
-curl --retry 99 -vfsL $url -o /tmp/kit.tar.gz
+curl --fail --retry 99 --silent --location $url --output /tmp/kit.tar.gz
 tar -zxvf /tmp/kit.tar.gz kit
 chmod +x kit
 sudo mv kit /usr/local/bin/kit
+
+echo "Installed /usr/local/bin/kit"
