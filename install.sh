96ea3eb956ebbdbe76ec9d06bad001cb3ca882b5
Alex Collins <alex_collins@intuit.com>
2025-01-05 14:59:24 -0800

    fix: fix url

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env sh
-set -eu
+set -eux
 
 os=$(uname | tr '[A-Z]' '[a-z]')
 arch=$(uname -m | sed 's/aarch64/arm64/' | sed 's/x86_64/amd64/')
 tag=${tag:-}
 
 if [ "$tag" = "" ]; then
-  curl --fail --retry 5 --silent --location https://api.github.com/repos/kitproj/kit/releases/latest --output /tmp/latest
-
-  tag=$(jq -r .tag_name /tmp/latest)
+  tag=$(curl --fail --retry 5 --silent --location https://api.github.com/repos/kitproj/kit/releases/latest | jq -r .tag_name)
 fi
 
-version=$(echo $tag | cut -c 2-)
-
-url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_${os}_${arch}"
-
-sudo curl --fail --retry 5 --silent --location $url --output /usr/local/bin/kit
+sudo curl --fail --retry 5 --silent --location --output /usr/local/bin/kit https://github.com/kitproj/kit/releases/download/${tag}/kit_${tag}_${os}_${arch}
 sudo chmod +x /usr/local/bin/kit
