46fe614511f819b2d2fa4cbab733a3a4397c998c
Alex Collins <alex_collins@intuit.com>
2025-01-05 14:04:42 -0800

    fix: release issues

--- install.sh
+++ install.sh
@@ -1,25 +1,19 @@
 #!/usr/bin/env sh
 set -eu
 
+os=$(uname | tr '[A-Z]' '[a-z]')
+arch=$(uname -m | sed 's/aarch64/arm64/')
 tag=${tag:-}
 
 if [ "$tag" = "" ]; then
-  # retry for all errors up to 10m
-  curl --fail --retry 99 --silent --location https://api.github.com/repos/kitproj/kit/releases/latest --output /tmp/latest
+  curl --fail --retry 5 --silent --location https://api.github.com/repos/kitproj/kit/releases/latest --output /tmp/latest
 
   tag=$(jq -r .tag_name /tmp/latest)
 fi
 
 version=$(echo $tag | cut -c 2-)
 
-echo "Downloading Kit $version"
+url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_${os}_${arch}"
 
-url="https://github.com/kitproj/kit/releases/download/${tag}/kit_${version}_$(uname)_$(uname -m | sed 's/aarch64/arm64/').tar.gz"
-
-# retry for all errors up to 10m
-curl --fail --retry 99 --silent --location $url --output /tmp/kit.tar.gz
-tar -zxvf /tmp/kit.tar.gz kit
-chmod +x kit
-sudo mv kit /usr/local/bin/kit
-
-echo "Installed /usr/local/bin/kit"
+sudo curl --fail --retry 5 --silent --location $url --output /usr/local/bin/kit
+sudo chmod +x /usr/local/bin/kit
