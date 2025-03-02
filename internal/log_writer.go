0b5e7e303e780cbf1cf9e2ab646dff883a6fb940
Alex Collins <alexec@users.noreply.github.com>
2025-03-02 08:01:09 -0800

    fix: correct broken prefix (#87)

--- internal/log_writer.go
+++ internal/log_writer.go
@@ -6,18 +6,18 @@ import (
 )
 
 type logWriter struct {
-	prefix string
-	suffix string
-	buffer bytes.Buffer
-	logger *log.Logger
+	// prefixSuffixProvider returns the prefix and suffix to use when logging.
+	prefixSuffixProvider func() (string, string)
+	buffer               bytes.Buffer
+	logger               *log.Logger
 }
 
 func (lw *logWriter) Write(p []byte) (int, error) {
-	// reset color and bold
+	prefix, suffix := lw.prefixSuffixProvider()
 
 	for _, b := range p {
 		if b == '\n' {
-			lw.logger.Printf("%s%s%s\n", lw.prefix, lw.buffer.String(), lw.suffix)
+			lw.logger.Printf("%s%s%s\n", prefix, lw.buffer.String(), suffix)
 			lw.buffer.Reset()
 		} else {
 			lw.buffer.WriteByte(b)
